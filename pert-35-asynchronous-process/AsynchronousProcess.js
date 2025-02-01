fc27db6822f49d538a075bb07a8d618f07b15506
Sunarto Utina <nartosunarto73126@gmail.com>
2025-02-01 14:09:54 +0700

    Belajar Asynchronous Process(Pengantar)

---
+++ pert-35-asynchronous-process/AsynchronousProcess.js
@@ -0,0 +1,57 @@
+// ASYNCHRONOUS PROCESS
+/*
+PENJELASAN: 
+
+Asynchronous memiliki arti tidak dalam waktu yang sama
+atau tidak serentak.
+
+Dalam pemrograman Asynchronous memiliki arti baris-baris kode
+yang menunggu untuk dieksekusi. Sementara menunggu, baris-baris
+kode lain dieksekusi. Setelah baris-baris kode lain dieksekusi,
+setelahnya baris kode yang menunggu tadi. 
+
+Asynchronous process memungkinkan kita untuk mengeksekusi kode
+secara pararel >> Maksudnya, misalnya kita memiliki 4 baris kode
+kode pertama dieksekusi karena syncronous, kode kedua dieksekusi
+namun dia asynchronous >> sehingga kode baris ketiga dieksekusi,
+namun baris kode ketiga juga asynchronous, sehingga menyebabkan
+sebuah eksekusi secara pararel, dua proces asynchronous dijalankan
+secara bersamaan. Setelahnya baris kode keempat dieksekusi(synchronous),
+setelahnya hasil dari asynchronous process yang pertama, setelahnya lagi
+hasil dari asynchronous process yang kedua.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
